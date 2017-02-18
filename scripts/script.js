2578a9015a923f5ca66a710caee4825b7159ad35
james sewell <jamesjosephsewell@gmail.com>
2017-02-17 18:52:46 -0600

    added a simple animated character

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ var userInput = window.setInterval(user_input, 100)
 var isHoveredOver = false
 var theTime = {hour: 0, minute: 0, second: 0}
 var backgroundHex = {red: "", green: "", blue: ""}
+var charTransform = 0
 
 
 function user_input(){
@@ -15,6 +16,14 @@ function user_input(){
 		displayElement.addEventListener("mouseout", mouseOutTime)
 	}
 
+	function mouseOverTime() {
+		isHoveredOver = true
+	}
+
+	function mouseOutTime() {
+		isHoveredOver = false
+	}
+
 }
 
 function main_loop(){
@@ -25,6 +34,8 @@ function main_loop(){
 
 	display_time()
 
+	club_goer_anim()
+
 }
 
 function define_time(){
@@ -34,8 +45,6 @@ function define_time(){
     theTime.hour = timeArray[0]
     theTime.minute = timeArray[1]
     theTime.second = timeArray[2]
-    
-    //console.log(timeArray)
       
 }
 
@@ -44,11 +53,9 @@ function define_bg_color(){
 	var newRandomValue = Math.floor((Math.random() * 127) + 1) + Math.floor((Math.random() * 127) + 1)
 	var bgColor = {r: Number(theTime.second*4) , g: "00", b: newRandomValue}
 	var bgColorHex = "#" + (bgColor.r).toString(16) + '00' + (bgColor.b).toString(16)
-	//console.log(bgColorHex)
-	backgroundHex.red = bgColorHex[1]+bgColorHex[2]
-	backgroundHex.green = bgColorHex[3]+bgColorHex[4]
-	backgroundHex.blue = bgColorHex[5]+bgColorHex[6]
-	//console.log(backgroundHex)
+	backgroundHex.red = (bgColor.r).toString(16)
+	backgroundHex.green = '00'
+	backgroundHex.blue = (bgColor.b).toString(16)
 	var page = document.getElementById("scene")
 	page.style.background = bgColorHex
 
@@ -57,27 +64,58 @@ function define_bg_color(){
 }
 
 function display_time(){
+
 	if (isHoveredOver === false){
 		var timeElement =  document.querySelector(".displayTime")
 		timeElement.innerHTML = theTime.hour + ":" + theTime.minute + ":" + theTime.second
 	}
+
 	else{
+
 		document.querySelector(".displayTime").innerHTML = backgroundHex.red + ":" + backgroundHex.green + ":" + backgroundHex.blue
 	}
 	console.log(isHoveredOver)
 }
 
+club_goer()
 
+function club_goer(){
+	
+	var charContainer = document.querySelector(".character")
+	var character = document.createElement("img")
+	character.src = "images/tmaster.gif"
+	character.id = "characterImg"
+	charContainer.appendChild(character)
 
-//var timeElout = document.querySelector(".displayTime").addEventListener("mouseout", mouseOutTime)
-
-function mouseOverTime() {
-	isHoveredOver = true
-    //document.querySelector(".displayTime").innerHTML = backgroundHex.red + ":" + backgroundHex.green + ":" + backgroundHex.blue
 }
 
-function mouseOutTime() {
-	isHoveredOver = false
-    //document.querySelector(".displayTime").innerHTML = theTime.hour + ":" + theTime.minute + ":" + theTime.second
+function club_goer_anim(){
+
+	var character = document.querySelector("#characterImg")
+	var charecterStyle = window.getComputedStyle(character)
+	var charContainer = document.querySelector(".character")
+	var charContainerStyle = window.getComputedStyle(charContainer)
+	
+	charTransform = charTransform + 1
+	console.log(charTransform)
+	charContainer.style.right = theTime.second*5+'px'
+
+	if (theTime.second > 3){
+
+		character.style.opacity = 1
+		
+	}
+
+	if (theTime.second > 42){
+		
+		charContainer.style.right = "30%"
+	}
+
+	if (theTime.second > 43){
+
+		character.style.opacity = 0
+		character.style.right = "10%"
+
+	}
 }
 
